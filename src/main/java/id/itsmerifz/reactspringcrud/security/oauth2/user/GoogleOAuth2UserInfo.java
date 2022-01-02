82f6c04295d441b218cae748aa6f6ada8261252e
itsmerifz <64636917+muhammadarief-17@users.noreply.github.com>
2022-01-02 12:52:21 +0700

    adding OAuth2 modules

---
+++ src/main/java/id/itsmerifz/reactspringcrud/security/oauth2/user/GoogleOAuth2UserInfo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package id.itsmerifz.reactspringcrud.security.oauth2.user;
+
+/**
+ *
+ * @author ACER
+ */
+public class GoogleOAuth2UserInfo {
+    
+}
